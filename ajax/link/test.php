a676eedc22909bf5e6938f0a3215240f77068379
nicole <15829617282@163.com>
2017-03-24 21:39:10 +0800

    first trying ajax

--- ajax/link/test.php
+++
@@ -1,4 +0,0 @@
-$name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
-$computedString = "Hi, " . $name;
-$array = ['userName' => $name, 'computedString' => $computedString];
-echo json_encode($array);
